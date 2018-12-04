0b06798147dbc04689d2f260092e14294b7703ee
Armando Jr <elfiservice@hotmail.com>
2018-12-04 08:38:50 -0300

    feat: provided Api using AsyncStorage

---
+++ src/utils/api.js
@@ -0,0 +1,64 @@
+import { AsyncStorage } from 'react-native'
+
+const DECK_STORAGE_KEY = 'StudyDeck:key'
+
+export const dataTest = {
+        React: {
+          title: 'React',
+          questions: [
+            {
+              question: 'What is React?',
+              answer: 'A library for managing user interfaces'
+            },
+            {
+              question: 'Where do you make Ajax requests in React?',
+              answer: 'The componentDidMount lifecycle event'
+            }
+          ]
+        },
+        JavaScript: {
+          title: 'JavaScript',
+          questions: [
+            {
+              question: 'What is a closure?',
+              answer: 'The combination of a function and the lexical environment within which that function was declared.'
+            }
+          ]
+        }
+      }
+
+export function saveDeckTitle(title) {
+    const newDeck = {
+        title,
+        questions: []
+    }
+    return AsyncStorage.mergeItem(DECK_STORAGE_KEY, JSON.stringify({
+        [title]: newDeck
+    }))
+}
+
+export function getDecks() {
+    return AsyncStorage.getItem(DECK_STORAGE_KEY)
+        .then( results => JSON.parse(results))
+}
+
+export function getDeck(title) {
+    return AsyncStorage.getItem(DECK_STORAGE_KEY)
+    .then((results) => {
+        const data = JSON.parse(results)
+        return data[title]
+    })
+}
+
+export function addCardToDeck(title, card) {
+    // card = { question: 'sadf', answer: 'sadf' }
+    return AsyncStorage.getItem(DECK_STORAGE_KEY)
+    .then((results) => {
+        const data = JSON.parse(results)
+        const deck = data[title]
+        deck.questions.push(card)
+        return AsyncStorage.mergeItem(DECK_STORAGE_KEY, JSON.stringify({
+            [title]: deck
+        }))
+    })
+}
