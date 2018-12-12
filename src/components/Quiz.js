932108d65e422aae99e829623a81a8055feb0ce1
Armando Jr <elfiservice@hotmail.com>
2018-12-12 07:58:02 -0300

    refactore: btns correct incorrect in Quiz

--- src/components/Quiz.js
+++ src/components/Quiz.js
@@ -4,15 +4,12 @@ import {
   Text, 
   View, 
   TouchableOpacity, 
-  TextInput, 
-  KeyboardAvoidingView,
-  Alert
 } from 'react-native';
-import { dark, red, green, darkLigth, white, gray } from '../utils/colors'
+import { red, green, darkLigth, white, gray } from '../utils/colors'
 import { connect } from 'react-redux'
 import { setQuiz, setAnswerToShow, setQuestionToShow, setAsCorrect, setAsIncorrect } from '../actions/quiz'
 import TextButton from '../templates/TextButton'
-import { FontAwesome, Ionicons } from '@expo/vector-icons'
+import { Ionicons, AntDesign } from '@expo/vector-icons'
 import QuizEnded from './QuizEnded'
 
 class Quiz extends React.Component {
@@ -28,7 +25,6 @@ class Quiz extends React.Component {
     }
 
     componentDidMount() {
-        //enviar questions para o Redux via action
         const { deck } = this.props.navigation.state.params
         this.props.setQuiz(deck.questions)
     }
@@ -55,13 +51,13 @@ class Quiz extends React.Component {
                 </View>
                 <View style={styles.header}>
                     <Text style={{fontSize: 30}}>{questionToShow.question} ?</Text>
+                    <View style={styles.textButton}>
+                        <TextButton style={{fontSize: 15}} onPress={() => this.props.setAnswerToShow(questionToShow) }>
+                            Answer
+                        </TextButton>
+                        <Ionicons name='ios-return-right' size={30} color={darkLigth} />
+                    </View>
                 </View>
-                <TouchableOpacity
-                    style={[styles.button, {backgroundColor: green}]}
-                    onPress={() => this.props.setAnswerToShow(questionToShow)} 
-                >
-                    <Text style={{color: white}}>Answer</Text>
-                </TouchableOpacity>
             </View>
         )
 
@@ -69,7 +65,6 @@ class Quiz extends React.Component {
 
     showAnwser(quiz) {
         const { show, numberOfQuestions, questions } = quiz
-        // console.log(show.answer);
         return (
             <View style={styles.container}>
                 <View style={styles.counter}>
@@ -77,23 +72,31 @@ class Quiz extends React.Component {
                 </View>
                 <View style={styles.header}>
                     <Text style={{fontSize: 30}}>{show.answer} </Text>
-                    <TextButton onPress={() => this.props.setQuestionToShow() }> 
+                    <View style={styles.textButton}>
+                        <TextButton style={{fontSize: 15}} onPress={() => this.props.setQuestionToShow() }> 
+                            Question
+                        </TextButton>                    
                         <Ionicons name='ios-return-left' size={30} color={darkLigth} />
-                        Question
-                    </TextButton>
+                    </View>
                 </View>
-                <TouchableOpacity
-                    style={[styles.button, {backgroundColor: green}]}
-                    onPress={() => this.props.setAsCorrect(show.number)} 
-                >
-                    <Text style={{color: white}}>Correct</Text>
-                </TouchableOpacity>
-                <TouchableOpacity
-                    style={[styles.button, {backgroundColor: red}]}
-                    onPress={() => this.props.setAsIncorrect(show.number)} 
-                >
-                    <Text style={{color: white}}>Incorrect</Text>
-                </TouchableOpacity>
+                <View style={styles.btns}>
+                    <TouchableOpacity
+                        style={[styles.button, {backgroundColor: green}]}
+                        onPress={() => this.props.setAsCorrect(show.number)} 
+                    >
+                        <Text style={{color: white}}>
+                            <AntDesign name='like1' size={30} color={white} />
+                        </Text>
+                    </TouchableOpacity>
+                    <TouchableOpacity
+                        style={[styles.button, {backgroundColor: red}]}
+                        onPress={() => this.props.setAsIncorrect(show.number)} 
+                    >
+                        <Text style={{color: white}}>
+                            <AntDesign name='dislike1' size={30} color={white} />
+                        </Text>
+                    </TouchableOpacity>
+                </View>
             </View>
         )
     }
@@ -128,15 +131,18 @@ const styles = StyleSheet.create({
     justifyContent: 'center'
   },
   btns: {
-    flex: 3,
-    marginLeft: 10,
-    marginRight: 10
+    flex: 1,
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'center',
   },
   button: {
     alignItems: 'center',
     backgroundColor: green,
-    padding: 15,
+    padding: 20,
     marginTop: 40,
+    marginLeft: 20,
+    marginRight: 20
   },
   textInput: {
     borderBottomWidth: 1,
@@ -145,7 +151,12 @@ const styles = StyleSheet.create({
     backgroundColor: darkLigth,
     fontSize: 20,
     marginBottom: 20,
-  },  
+  },
+  textButton: {
+    marginTop: 30,
+    justifyContent: 'center',
+    alignItems: 'center'
+  }
 });
 
 function mapStateToProps({ quiz }) {
